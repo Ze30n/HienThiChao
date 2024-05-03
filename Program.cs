702ea299686daff2a1685f403ab355097f919e93
nguyen xuan tien <nguyen.xuan.tien096@gmail.com>
2024-05-03 16:09:40 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ho va ten");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello: " + name);
+    }
+}
